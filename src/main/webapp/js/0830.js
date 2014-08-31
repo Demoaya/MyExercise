fdbde0d489643a858f09daa2e52e30d66631d744
Demoaya <willsonmarx@163.com>
2014-08-31 17:15:25 +0800

    learn to use jQuery html ,and study jQuery traversal

--- src/main/webapp/js/0830.js
+++ src/main/webapp/js/0830.js
@@ -1,18 +1,69 @@
 $(document).ready(function(){
 	$("img#img").mouseover(function(){
-		$("#timer").animate({left:"300px",fontSize:"2em"});
+		$("#timer").animate({left:"300px",fontSize:"3em"});
 	})
 });
 
 $(document).ready(function(){
 	$("button#bott").click(function(){
-		$("p#slide").fadeOut(100)
+		$("a.button").toggle()
 	})
 });
 
 $(document).ready(function(){
     $("a.button").mouseover(function(){
-    	$(this).fadeOut(500)
+    	$("p#slide").fadeToggle(1000)
     })
 });
- 
+$(document).ready(function(){
+    $("#search").click(function(){
+    	//$("this").attr("href","#url".val())
+    	window.location.href=$("#url").text()
+    })
+});
+$(document).ready(function(){
+    $("#red").click(function(){
+    	//$("a.button").attr("background","#ff0000")
+    	$("a.button").css("color","red")
+    })
+});
+$(document).ready(function(){
+    $("#size").click(function(){
+    	//$("a.button").attr("font-size","24px")
+    	$("a.button").css("font-size","24px")
+    })
+});
+$(document).ready(function(){
+    $("#chtext").click(function(){
+    	$("a.button").text("Yeah!")    	
+    })
+});
+
+$(document).ready(function(){
+    $("#append").click(function(){
+    	var yeah=$("<p></p>").html("<h2>Yeah!</h2>");
+      	$("#chtext").append(yeah);
+    })
+});
+
+$(document).ready(function(){
+    $("#remove").click(function(){
+      	$("#chtext").empty();
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
